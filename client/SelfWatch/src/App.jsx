0e1c4c8166d430a0a88c0cf0a1c3a93418b7d7c1
LadyAudrey <theaudreygreene@gmail.com>
2023-08-05 19:47:19 -0700

    adding CSS and creating state within the UserForm

--- client/SelfWatch/src/App.jsx
+++ client/SelfWatch/src/App.jsx
@@ -12,21 +12,24 @@ import { NewItem } from "./pages/NewItem";
 import Container from "react-bootstrap/esm/Container";
 import Card from "react-bootstrap/Card";
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <>
         <Container>
           <Header />
-          <Card className="bg-primary m-5 border border-trim-rounded rounded-5 border-5">
-            <Routes>
-              <Route path="/" element={<FirstEntry />} />
-              <Route path="/new_item" element={<NewItem />} />
-              <Route path="/item" element={<Item />} />
-              <Route path="/selfcaretasks" element={<SelfCareTasks />} />
-              <Route path="/preferences" element={<Preferences />} />
-            </Routes>
-          </Card>
+          <div className="d-flex flex-column align-items-center">
+            <h2 className="m-5">Page Item</h2>
+            <Card className="bg-primary border border-trim-rounded rounded-5 border-5 w-100 align-items-center">
+              <Routes>
+                <Route path="/" element={<FirstEntry />} />
+                <Route path="/new_item" element={<NewItem />} />
+                <Route path="/item" element={<Item />} />
+                <Route path="/selfcaretasks" element={<SelfCareTasks />} />
+                <Route path="/preferences" element={<Preferences />} />
+              </Routes>
+            </Card>
+          </div>
         </Container>
       </>
     );
