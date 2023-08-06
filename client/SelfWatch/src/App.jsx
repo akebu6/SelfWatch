9001e7156bd906ad33abff3c599542be3dbacbc1
akebu6 <asimasiku999@gmail.com>
2023-08-06 12:03:27 +0200

    task: added authentication

--- client/SelfWatch/src/App.jsx
+++ client/SelfWatch/src/App.jsx
@@ -40,3 +40,36 @@ class App extends React.Component {
 }
 
 export default App;
+
+/**
+ * import { useAuth0 } from '@auth0/auth0-react';
+// import LoginButton from './components/login';
+// import LogoutButton from './components/Logout';
+// import Profile from './components/Profile';
+
+function App() {
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
+
+  return (
+    <div>
+      <header>
+        <h1>My Vite React App</h1>
+        {isAuthenticated ? (
+          <>
+            <div>
+              Welcome, {user.name}!{' '}
+              <button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>
+            </div>
+          </>
+        ) : (
+          <button onClick={loginWithRedirect}>Log In</button>
+        )}
+      </header>
+      <main>
+        </main>
+        </div>
+      );
+    }
+    
+    export default App;
+ */
